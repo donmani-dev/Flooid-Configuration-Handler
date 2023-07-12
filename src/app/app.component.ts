ce1004c56a378b7fed4f048f9ea5e17b6ffd55ce
donmani-dev <abdultubb10@gmail.com>
2023-07-12 10:07:36 +0500

    making calls to Sprint boot login API through Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,39 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent {
+  protected username: string = "";
+  protected password: string = "";
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
+  setUsername(event: any) {
+    this.username = event.target.value;
+  }
+
+  setPassword(event: any) {
+    this.password = event.target.value;
+  }
+
+  getFriends() {
+    if (!this.username || !this.password) {
+      console.log("Please enter a username and password.");
+      return;
+    }
+
+    const url = `http://localhost:8080/getUser/${this.username}/${this.password}`;
+
+    this.httpClient.get(url, { responseType: "text" }).subscribe(
+      (response: any) => {
+        console.log(response);
+      },
+      (error: any) => {
+        console.log("Error occurred during the HTTP request:", error);
+      }
+    );
+  }
 }
+
+
+
